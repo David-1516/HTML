cf7b1a29aaf9bc8c86e8dfaba801abae6631475b
David Bolic <davidbolic03@gmail.com>
2024-06-26 11:46:06 +0200

    no message

--- ReactAxios/react-hooks-crud/src/components/Grid.js
+++ ReactAxios/react-hooks-crud/src/components/Grid.js
@@ -4,7 +4,21 @@ import StudentsList from './StudentsList';
 
 function Grid() {
   return (
-    <div className="container mt-4">
+    
+    <div>
+             <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/">Student Management</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/grid">Grid</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div  className="container mt-4">
       <div className="row">
         <div className='col-sm-5'></div>
         <div className='col-sm-4'><h1 className="mb-5 ">Students</h1></div>
@@ -12,6 +26,7 @@ function Grid() {
       </div>
       <StudentsList />
       <Link to="/add-student" className="btn btn-primary mb-4">Add Student</Link>
+      </div>
     </div>
   );
 }
